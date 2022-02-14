c94e6fa1defe391077b102b807ad39cd1a2de49f
tsachkova <tsacykova@gmail.com>
2022-02-14 17:58:37 +0200

    all

--- js/indepth.js
+++ js/indepth.js
@@ -1,9 +1,27 @@
-function myMap(arr, callback) {
+Array.prototype.myMap = function(callback) {
     let newArr = [];
-
-    for(let i = 0; i < arr.length; i++) {
-        newArr.push(callback(arr[i], i, arr))
+    let myArr = this;
+    
+    for(let i = 0; i < myArr.length; i++) {
+        newArr.push(callback(myArr[i], i, myArr))
     }
+
     return newArr;
 
 }
+
+Array.prototype.myFilter = function(callback) {
+    let newArr = [];
+    let myArr = this;
+    
+    for(let i = 0; i < myArr.length; i++) {
+        let result = callback(myArr[i], i, myArr);
+        if(Boolean(result)) {
+            newArr.push(myArr[i])
+        }
+    }
+
+    return newArr;
+
+}
+
