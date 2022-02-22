0d350d811c0983d365539b1299add58ac0792ecc
tsachkova <tsacykova@gmail.com>
2022-02-22 15:05:04 +0200

    all

--- js/algorithm.js
+++ js/algorithm.js
@@ -1,219 +1,253 @@
-
-
-
-
 function sortObject(elementPrev, elementNext, property) {
     return (elementPrev[property] > elementNext[property]);
 }
 
-function sortPrimitive(elementPrev, elementNext) {
+function sortPrimitiveArr(elementPrev, elementNext) {
     return (elementPrev > elementNext);
 }
 
-Array.prototype.insertionSort = function(callback, property) {
-    let temp;
+Array.prototype.insertionSort = function (callback, property) {
+    if (typeof callback !== 'function') {
+        throw new Error("callback is not a function");
+    }
+
+    let tempStorage;
+
     for (let i = 1; i < this.length; i++) {
         sortNumber = this[i];
+        
         for (let j = 0; j < i; j++) {
             
-            // if (arr[j] > arr[i])
-            if (callback(this[j], this[i], property))
-            {
-                
-                temp = this[i];
+            if (callback(this[j], this[i], property)) {
+                tempStorage = this[i];
                 for (let m = i; m > j; m--) {
                     this[m] = this[m - 1];
                 }
-                this[j] = temp;
+                this[j] = tempStorage;
             }
-
         }
     }
-    return console.log(this)
-
+    return this;
 }
 
-Array.prototype.bubbleSort = function(callback, property) {
-    let count = 0;
-    let temp;
-    for(let i = 0; true; i++) {
-        for(let j = 0; j < this.length - 1; j++) {
-            if(callback(this[j],this[j + 1], property)) {
+Array.prototype.bubbleSort = function (callback, property) {
+    if (typeof callback !== 'function') {
+        throw new Error("callback is not a function");
+    }
 
-                    temp = this[j];
-                    this[j] = this[j+1];
-                    this[j+1] = temp;
-                    ++count;
-                    console.log(this[j])
-                
+    let count = 0;
+    let tempStorage;
+
+    for (let i = 0; true; i++) {
+
+        for (let j = 0; j < this.length - 1; j++) {
+
+            if (callback(this[j], this[j + 1], property)) {
+                tempStorage = this[j];
+                this[j] = this[j + 1];
+                this[j + 1] = tempStorage;
+                ++count;
             }
-        }console.log(count)
+        }
+
         if (count === 0) {
             return this;
         }
-        count = 0;
 
-    }    
+        count = 0;
+    }
 }
 
 
 
-    let root;
-    function bynaryTree(number) {
-        class Node {
-            constructor(number) {
-                this.number = number;
-                this.left = 0;
-                this.right = 0;
-            }
-        }
-        
-        if (root === undefined) {
-            root = new Node (number);
-        } else {
-            let way = root;
-            for(let i = 0; true; i++ ) {
-                if (number > way.number){
-                    
-                    if (way.right !==0) {
+let root;
 
-                        way = way.right;
-                    } else { way.right = new Node(number);
-                            break;
-                    }
-                }
-                if (number < way.number){
-                    if (way.left !== 0)  {
-                        way = way.left;
-                    } else { way.left = new Node(number);
-                            break;
-                    }    
-                    
-                }
-          
-            }
-            // way = new Node (number);
-        }    
-            
-        return root;
+function binaryTreeAdd(number) {
+    if (typeof number !== 'number') {
+        throw new Error("Wrong arguments type");
     }
 
+    class Node {
+        constructor(number) {
+            this.number = number;
+            this.left = 0;
+            this.right = 0;
+        }
+    }
 
-
-
-function BynarySearch(number) {
+    if (root === undefined) {
+        root = new Node(number);
+        return root;
+    } 
+    
     let way = root;
-    for(let i = 0; true; i++ ) {
-        if(way.number === number) {
+
+    for (let i = 0; true; i++) {
+
+        if (number > way.number) {
+            if (way.right !== 0) {
+                way = way.right;
+            } else {
+                way.right = new Node(number);
+                return root;
+            }
+        }
+
+        if (number < way.number) {
+            if (way.left !== 0) {
+                way = way.left;
+            } else {
+                way.left = new Node(number);
+                return;
+            }
+        }
+
+        if (number === way.number) {
+            throw new Error("number is duplicated");
+        }      
+    }
+}
+
+function binarySearch(number) {
+    if (typeof number !== 'number') {
+        throw new Error("Wrong arguments type");
+    }
+
+    let way = root;
+    for (let i = 0; true; i++) {
+
+        if (way.number === number) {
             return way;
         }
 
-        if (number > way.number){            
-            if (way.right !==0) {
+        if (number > way.number) {
+            if (way.right !== 0) {
                 way = way.right;
-            } else { 
+            } else {
                 throw new Error("Number not found");
             }
         }
 
-        if (number < way.number){
-            if (way.left !== 0)  {
+        if (number < way.number) {
+            if (way.left !== 0) {
                 way = way.left;
-            } else { 
+            } else {
                 throw new Error("Number not found");
-            }    
-            
+            }
         }
-  
     }
 }
 
-function bynaryDelete(number, way, parent){
-    parent = parent || undefined;
-    way = way || root;
-   
+function binaryDelete(number, way, parent) {
+    if (typeof number !== 'number') {
+        throw new Error("Wrong arguments type");
+    }
     
-        if (number > way.number){            
-            if (way.right !==0) {
-                parent = way;
-                way = way.right;
-            } else { 
-                throw new Error("Number not found");
-            }
+    way = way || root;
 
+    if (number > way.number) {
+        if (way.right !== 0) {
+            parent = way;
+            way = way.right;
+        } else {
+            throw new Error("Number not found");
         }
-        if (number < way.number){            
-            if (way.left !==0) {
-                parent = way;
-                way = way.left;
+    }
 
-            } else { 
-                throw new Error("Number not found");
+    if (number < way.number) {
+
+        if (way.left !== 0) {
+            parent = way;
+            way = way.left;
+        } else {
+            throw new Error("Number not found");
+        }
+    }
+
+    if (number === way.number) {
+        if ((way.left === 0) && (way.right === 0)) {
+            if(way === root) {
+                root = 0;
+                return;
             }
+            if (way.number < parent.number) {
+                parent.left = 0;
+            }
+
+            if (way.number > parent.number) {
+                parent.right = 0;
+            }
+
+            return;
         }
 
-        if(number === way.number) {
-            if((way.left === 0) && (way.right === 0)) {
-                
-                if(way.number < parent.number) {
-                    parent.left = 0;
-                    
-                }
-                if(way.number > parent.number) {
-                    parent.right = 0;
-                    
-                }
+        if ((way.left === 0) && (way.right !== 0)) {
+            if(way === root) {
+                root.number = root.right.number;
+                root.left = root.right.left;
+                root.right = root.right.right;
                 return;
-
-            } 
-            if((way.left === 0) && (way.right !== 0)) {
-                if(way.number < parent.number) {
-                    parent.left = way.right;
-                    
-                }
-                if(way.number > parent.number) {
-                    parent.right = way.right;
-                   
-                }
-                return;
-
             }
 
-            if((way.left !== 0) && (way.right === 0)) {
-                if(way.number < parent.number) {
-                    parent.left = way.left;
-                    
-                }
-                if(way.number > parent.number) {
-                    parent.right = way.left;
-                    
-                }
-                return
-
+            if (way.number < parent.number) {
+                parent.left = way.right;
             }
 
-            if((way.left !== 0) && (way.right !== 0)) {
-                let minElement = way.right;
-                let minElementPerent;
-                for(i = 0; true; i++) {
-                    if(minElement.left !== 0) {
-                        minElementPerent = minElement;
-                        minElement = minElement.left;
-                        console.log(minElement);} 
-                    if (minElement.left === 0) {
-                        console.log(minElement);
-                        way.number = minElement.number;
-                        if(minElement.right !== 0) {
-                            minElementPerent.right = minElement.right;
-                        }
-                        return
+            if (way.number > parent.number) {
+                parent.right = way.right;
+            }
+
+            return;
+        }
+
+        if ((way.left !== 0) && (way.right === 0)) {
+            if(way === root) {
+                root.number = root.left.number;
+                root.left = root.left.left;
+                root.right = root.left.right;
+                return;
+            }
+
+            if (way.number < parent.number) {
+                parent.left = way.left;
+            }
+
+            if (way.number > parent.number) {
+                parent.right = way.left;
+            }
+
+            return;
+        }
+
+        if ((way.left !== 0) && (way.right !== 0)) {
+            let minElement = way.right;
+            let minElementPerent  = way;
+
+            for (i = 0; true; i++) {
+            
+                if (minElement.left !== 0) {
+                    minElementPerent = minElement;
+                    minElement = minElement.left;
+                }
+
+                if (minElement.left === 0) {
+                    way.number = minElement.number;
+                    if(!(minElementPerent === way)) {
+                        minElementPerent.left = 0;
+                    } else {
+                        way.right = minElementPerent.right;
                     }
 
-                }
+                    if (minElement.right !== 0) {
+                        minElementPerent.left = minElement.right;
+                    } 
 
+                    return;
+                }
             }
-    
         }
-        return bynaryDelete(number, way, parent);
     }
+
+    return binaryDelete(number, way, parent);
+}
 
